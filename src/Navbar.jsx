863044ea44a4a5426ee1e55f74f78576ee7d5f02
Ephraimiyanda <iyandaephraim@gmail.com>
2023-06-05 10:29:27 -0900

    countries name width increased and country-details gap increased

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { useContext } from "react"
 import { CountryContext } from "./countryContext"
 import sun from "../images/icon-sun.svg"
@@ -10,17 +10,4 @@ export default function Navbar(){
              <header className={Theme? "  light-header":''}><div>Where in the world?</div> <div className="mode"><span onClick={handleThemeSwitch}><img src={Theme?moon:sun} alt="" /> {Theme?"Dark mode":"Light mode"}</span></div></header>
         </div>
     )
-=======
-import { useContext } from "react"
-import { CountryContext } from "./countryContext"
-import sun from "../images/icon-sun.svg"
-import moon from "../images/icon-moon.svg"
-export default function Navbar(){
-  const{handleThemeSwitch,Theme}=useContext(CountryContext)
-    return(
-        <div className="nav">
-             <header className={Theme? "  light-header":''}><div>Where in the world?</div> <div className="mode"><span onClick={handleThemeSwitch}><img src={Theme?moon:sun} alt="" /> {Theme?"Darkmode":"Light mode"}</span></div></header>
-        </div>
-    )
->>>>>>> e85b46239d38648df7293fcb804617bb37c40ea9
 }
